1fd77d2ef61f25ef82e9445023614637467568da
AlirezaHabibi1377 <110274879+AlirezaHabibi1377@users.noreply.github.com>
2024-07-31 19:09:54 +0330

    Update orange_prediction.py

--- orange_prediction.py
+++ orange_prediction.py
@@ -2,30 +2,47 @@ import cv2
 from orange_detector import OrangeDetector
 from kalmanfilter import KalmanFilter
 
-cap = cv2.VideoCapture("E:\Perception Working-Youtube videos and Github\Kalman filter for trajectory\Pysource-Kalman-filter\Pysource Kalman filter\orange.mp4")
+# Open the video file
+cap = cv2.VideoCapture("orange.mp4")
 
-# Load detector
+# Initialize the OrangeDetector for detecting oranges
 od = OrangeDetector()
 
-# Load Kalman filter to predict the trajectory
+# Initialize the KalmanFilter for predicting the trajectory of the orange
 kf = KalmanFilter()
 
 while True:
+    # Read a frame from the video
     ret, frame = cap.read()
     if ret is False:
-        break
+        break # Break the loop if no more frames are available
 
+    # Detect the orange in the current frame and get its bounding box
     orange_bbox = od.detect(frame)
     x, y, x2, y2 = orange_bbox
+
+    # Calculate the center coordinates of the detected orange
     cx = int((x + x2) / 2)
     cy = int((y + y2) / 2)
 
+    # Predict the next position of the orange using the Kalman filter
     predicted = kf.predict(cx, cy)
-    #cv2.rectangle(frame, (x, y), (x2, y2), (255, 0, 0), 4)
+
+    # Draw a circle at the current center of the detected orange
     cv2.circle(frame, (cx, cy), 20, (0, 0, 255), 4)
+
+    # Draw a circle at the predicted position of the orange
     cv2.circle(frame, (predicted[0], predicted[1]), 20, (255, 0, 0), 4)
 
+    # Display the frame with the detected and predicted positions
     cv2.imshow("Frame", frame)
+
+    # Wait for 150 milliseconds and check if the 'Esc' key is pressed
     key = cv2.waitKey(150)
-    if key == 27:
-        break
+    if key == 27: # ASCII code for the 'Esc' key
+        break # Break the loop if the 'Esc' key is pressed
+
+
+# Release the video capture object and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
